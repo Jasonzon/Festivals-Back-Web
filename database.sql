917b60dd8165aedc3f02879e362a4d82369e91bd
Jason <jazzmoret@gmail.com>
2023-02-02 10:24:05 +0100

    database

---
+++ database.sql
@@ -0,0 +1,37 @@
+create database festivals;
+
+create table benevole(
+    benevole_id serial primary key,
+    benevole_prenom varchar(255) not null,
+    benevole_nom varchar(255) not null,
+    benevole_mail varchar(255) unique not null
+);
+
+create type type_jeu as enum ('enfant','famille','ambiance','initié','expert');
+
+create table jeu(
+    jeu_id serial primary key,
+    jeu_type type_jeu not null
+);
+
+create table zone(
+    zone_id serial primary key,
+    zone_name varchar(255) not null
+);
+
+create table affectation(
+    affectation_id serial primary key,
+    affectation_jeu int not null,
+    foreign key (affectation_jeu) references jeu(jeu_id),
+    affectation_zone int not null,
+    foreign key (affectation_zone) references zone(zone_id)
+);
+
+create table travail(
+    travail_id serial primary key,
+    travail_benevole int not null,
+    foreign key (travail_benevole) references benevole(benevole_id),
+    travail_zone int not null,
+    foreign key (travail_zone) references zone(zone_id),
+    travail_creneau varchar(255) not null
+);
--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const PORT = process.env.PORT || 5000
+const path = require('path');
 
 app.use(cors())
 app.use(express.json())
+
+/*
+app.get("/*", (req,res) => {
+    res.sendFile(path.join(__dirname,"client/build/index.html"))
+})
+*/
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
