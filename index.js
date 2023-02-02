71b2ef20c67914bf564482f2b95a2bd7293b3252
Jason <jazzmoret@gmail.com>
2023-02-02 10:48:08 +0100

    route benevole

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const path = require('path');
 app.use(cors())
 app.use(express.json())
 
+app.use("/jeu", require("./routes/jeu"))
+
+app.use("/benevole", require("./routes/benevole"))
+
+app.use("/zone", require("./routes/zone"))
+
+app.use("/affectation", require("./routes/affectation"))
+
+app.use("/travail", require("./routes/travail"))
+
 /*
 app.get("/*", (req,res) => {
     res.sendFile(path.join(__dirname,"client/build/index.html"))
--- routes/benevole.js
+++ routes/benevole.js
@@ -1,2 +1,74 @@
 const router = require("express").Router()
 const pool = require("../db")
+
+router.get("/", async (req,res) => {
+    try {
+        const allBenevoles = await pool.query("select * from benevole")
+        return res.json(allBenevoles.rows).status(200)
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+router.get("/:id", async (req,res) => {
+    try {
+        const {id} = req.params
+        const benevole = await pool.query("select * from benevole where benevole_id = $1",[id])
+        if (benevole.rows.length === 0) {
+            return res.status(404)
+        }
+        else {
+            return res.json(benevole.rows[0]).status(200)
+        }
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+router.post("/", async (req,res) => {
+    try {
+        const {prenom,nom,mail} = req.body
+        const benevole = await pool.query("insert into benevole (benevole_prenom,benevole_nom,benevole_mail) values ($1, $2, $3) returning *",[prenom,nom,mail])
+        if (benevole.rows.length === 0) {
+            return res.status(500)
+        }
+        else {
+            return res.json(benevole.rows[0]).status(200)
+        }
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+router.put("/:id", async (req,res) => {
+    try {
+        const {id} = req.params
+        const {prenom,nom,mail} = req.body
+        const benevole = await pool.query("update benevole set benevole_prenom = $2, benevole_nom = $3, benevole_mail = $4 where benevole_id = $1 returning *",[id,prenom,nom,mail])
+        if (benevole.rows.length === 0) {
+            return res.status(500)
+        }
+        else {
+            return res.json(benevole.rows[0]).status(200)
+        }
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+router.delete("/:id", async (req,res) => {
+    try {
+        const {id} = req.params
+        const benevole = await pool.query("delete from benevole where benevole_id = $1 returning *",[id])
+        if (benevole.rows.length === 0) {
+            return res.status(500)
+        }
+        else {
+            return res.status(200)
+        }
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+module.exports = router
